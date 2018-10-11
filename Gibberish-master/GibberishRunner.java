4ea7b5f60892a77a1385c5f39b62f5fe35c30803
Steven Nguyen <37124470+snguyen01@users.noreply.github.com>
2018-10-11 10:31:37 -0700

    Add files via upload

---
+++ Gibberish-master/GibberishRunner.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class GibberishRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GibberishRunner
+{
+    public static void main()
+    {
+       String wordlist5000 = new String();
+       String gibberish = new String();
+       
+    }
+
+}
