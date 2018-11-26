dde997fe3d8fc0c7cd91f8fe13abb233836e701d
Steven Nguyen <37124470+snguyen01@users.noreply.github.com>
2018-11-26 09:34:41 -0800

    1126

---
+++ Lab1126 CellMatrix/Cell.java
@@ -0,0 +1,32 @@
+
+/**
+ * @author (eettlin) 
+ * @version (11 26 18)
+ */
+public class Cell
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private Cell[] neighbors;
+
+    /**
+     * Constructor for objects of class Cell
+     */
+    public Cell(int a)    {
+        x = a;
+        neighbors = new Cell[4];// North, East, South, and West only 
+    }
+
+    /**
+     * ++++++++++++++  methods
+     */
+    
+    //  Load neighbor cells into the neighbors array
+    public void loadNeighbors(){
+        
+    }
+    
+    public int getInt(){
+        return x;
+    }
+}
---
+++ Lab1126 CellMatrix/CellMatrix.java
@@ -0,0 +1,42 @@
+
+/**
+ * 
+ * CellMatrix
+ * @author (eettlin) 
+ * @version (11-26-18)
+ */
+public class CellMatrix{
+    /**
+     * +++++++++++++++++++++Instance variables
+     */
+    
+    private Cell[][] cells;
+
+    /**
+     * +++++++++++++++++++++Constructor for objects of class NumberMatrix
+     */
+    public CellMatrix(int a, int b){
+       cells = new Cell[a][b];
+       
+    }
+
+    /**
+     * +++++++++++++++++++++++++++++++++++++++++++++++++++++++methods
+     */
+    //Print each row and column of nums to the console
+    public void printMatrix(){
+       
+    }
+    //  load nums with random with numbers between 1 and 10 inclusive
+    public void loadMatrix(){
+       
+    }
+    //  Findis the sum of each of the neighbors and returns the greatest 
+    // neighbor value
+    public int getGreatestNeighbors(){
+       
+        return 0;
+    }
+    
+    
+}
